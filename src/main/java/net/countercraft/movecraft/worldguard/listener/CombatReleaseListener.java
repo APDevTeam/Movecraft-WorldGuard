d055aa9d242c5db4b739b03a51884945709d9935
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-12-28 16:05:11 -0600

    Bump version and Combat refactor

--- src/main/java/net/countercraft/movecraft/worldguard/listener/CombatReleaseListener.java
+++ src/main/java/net/countercraft/movecraft/worldguard/listener/CombatReleaseListener.java
@@ -1,6 +1,6 @@
 package net.countercraft.movecraft.worldguard.listener;
 
-import net.countercraft.movecraft.combat.event.CombatReleaseEvent;
+import net.countercraft.movecraft.combat.features.combat.events.CombatReleaseEvent;
 import net.countercraft.movecraft.worldguard.MovecraftWorldGuard;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
