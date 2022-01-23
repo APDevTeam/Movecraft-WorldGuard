e8af96ccfbc7a0f405063caa58ca2213de19eaee
TylerS1066 <TylerS1066@users.noreply.github.com>
2022-01-23 15:05:49 -0600

    Update ExplosionListener.java

--- src/main/java/net/countercraft/movecraft/worldguard/listener/ExplosionListener.java
+++ src/main/java/net/countercraft/movecraft/worldguard/listener/ExplosionListener.java
@@ -1,5 +1,6 @@
 package net.countercraft.movecraft.worldguard.listener;
 
+import com.sk89q.worldguard.protection.flags.Flags;
 import net.countercraft.movecraft.events.ExplosionEvent;
 import net.countercraft.movecraft.worldguard.MovecraftWorldGuard;
 import org.bukkit.event.EventHandler;
@@ -8,7 +9,7 @@ import org.bukkit.event.Listener;
 public class ExplosionListener implements Listener {
     @EventHandler
     public void onExplosion(ExplosionEvent e) {
-        if(MovecraftWorldGuard.getInstance().getWGUtils().isOtherExplosionAllowed(e.getExplosionLocation()))
+        if(!MovecraftWorldGuard.getInstance().getWGUtils().isFlagDenied(e.getExplosionLocation(), Flags.OTHER_EXPLOSION))
             return;
 
         e.setCancelled(true);
