9a5de5c47634ac0154331a9409054a7657b8291a
TylerS1066 <TylerS1066@users.noreply.github.com>
2022-01-23 15:05:32 -0600

    Reorganize flags

--- src/main/java/net/countercraft/movecraft/worldguard/utils/CustomFlags.java
+++ src/main/java/net/countercraft/movecraft/worldguard/utils/CustomFlags.java
@@ -4,9 +4,9 @@ import com.sk89q.worldguard.protection.flags.RegionGroup;
 import com.sk89q.worldguard.protection.flags.StateFlag;
 
 public class CustomFlags {
-    public static final StateFlag ALLOW_TRANSLATE = new StateFlag("allow-translate", true, RegionGroup.MEMBERS);
-    public static final StateFlag ALLOW_ROTATE = new StateFlag("allow-rotate", true, RegionGroup.MEMBERS);
-    public static final StateFlag ALLOW_COMBAT_RELEASE = new StateFlag("allow-combat-release", true, RegionGroup.MEMBERS);
-    // public static final StateFlag ALLOW_CRAFT_SINK = new StateFlag("allow-craft-sink",
+    public static final StateFlag ALLOW_COMBAT_RELEASE = new StateFlag("allow-combat-release", true, RegionGroup.ALL);
+    public static final StateFlag ALLOW_CRAFT_SINK = new StateFlag("allow-craft-sink", false, RegionGroup.ALL);
     public static final StateFlag ALLOW_CRAFT_PILOT = new StateFlag("allow-craft-pilot", true, RegionGroup.MEMBERS);
+    public static final StateFlag ALLOW_ROTATE = new StateFlag("allow-rotate", true, RegionGroup.MEMBERS);
+    public static final StateFlag ALLOW_TRANSLATE = new StateFlag("allow-translate", true, RegionGroup.MEMBERS);
 }
