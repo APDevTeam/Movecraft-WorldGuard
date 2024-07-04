407f347a9ca1a1cbc46fa3b304e1326be859f89a
TylerS1066 <TylerS1066@users.noreply.github.com>
2024-07-04 15:07:14 -0500

    Gradle (#14)
    * Update .gitignore
    
    * Gradle auto init
    
    * Switch over build system
    
    * Changing permission of gradlew

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Movecraft-WorldGuard
-![WorldGuard](https://github.com/APDevTeam/Movecraft-WorldGuard/actions/workflows/maven.yml/badge.svg)
+![WorldGuard](https://github.com/APDevTeam/Movecraft-WorldGuard/actions/workflows/gradle.yml/badge.svg)
 [![Codebeat](https://codebeat.co/badges/995849d2-3017-4a93-a9bb-2dcf00c661bd)](https://codebeat.co/projects/github-com-apdevteam-movecraft-worldguard-main)
 
 Home of the code for the following features:
@@ -13,20 +13,18 @@ The `1.12.2` branch is coded for Movecraft 7.x and WorldGuard v6.2.x for 1.12.2.
 The `main` branch is coded for Movecraft 8.x and WorldGuard v7.x for 1.14.4+.
 
 ## Download
+Releases can be found on the [Releases tag](https://github.com/APDevTeam/Movecraft-WorldGuard).
 
-Devevlopment builds can be found on the [Releases page](https://github.com/APDevTeam/Movecraft-WorldGuard) of this repository.
+Development builds can be found on the [GitHub Actions tab](https://github.com/APDevTeam/Movecraft-WorldGuard/actions) of this repository.
 
-Stable builds can be found on [our SpigotMC page](https://www.spigotmc.org/resources/movecraft-worldguard.90428/).
+Previous builds can be found on [our SpigotMC page](https://www.spigotmc.org/resources/movecraft-worldguard.90428/).
 
 ## Building
-This plugin requires that the user setup and build their [Movecraft-Combat](https://github.com/TylerS1066/Movecraft-Combat) development environment, and then clone this into the same folder as your Movecraft-Combat development environment such that Movecraft-WorldGuard and Movecraft-Combat are contained in the same folder.
-
-Then, run the following to build Movecraft-WorldGuard through `maven`.
+Run the following to build Movecraft-WorldGuard:
 ```
-mvn clean install
+./gradlew clean build --parallel
 ```
-Jars are located in `/target`.
-
+Jars are located in `/build/libs` directory.
 
 ## Support
 [Github Issues](https://github.com/APDevTeam/Movecraft-WorldGuard/issues)
---
+++ build.gradle.kts
@@ -0,0 +1,91 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    id("io.github.0ffz.github-packages") version "1.2.1"
+    id("io.papermc.hangar-publish-plugin") version "0.1.2"
+}
+
+repositories {
+    gradlePluginPortal()
+    mavenLocal()
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven { githubPackage("apdevteam/movecraft")(this) }
+    maven { githubPackage("apdevteam/movecraft-combat")(this) }
+    maven("https://maven.enginehub.org/repo/")
+}
+
+dependencies {
+    api("org.jetbrains:annotations-java5:24.1.0")
+    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+    compileOnly("net.countercraft:movecraft:+")
+    compileOnly("net.countercraft.movecraft.combat:movecraft-combat:+")
+    api("com.sk89q.worldedit:worldedit-core:7.2.9")
+    api("com.sk89q.worldguard:worldguard-bukkit:7.0.7")
+}
+
+group = "net.countercraft.movecraft.worldguard"
+version = "1.0.0_beta-3_gradle"
+description = "Movecraft-WorldGuard"
+java.toolchain.languageVersion = JavaLanguageVersion.of(17)
+
+tasks.jar {
+    archiveBaseName.set("Movecraft-WorldGuard")
+    archiveClassifier.set("")
+    archiveVersion.set("")
+}
+
+tasks.processResources {
+    from(rootProject.file("LICENSE.md"))
+    filesMatching("*.yml") {
+        expand(mapOf("projectVersion" to project.version))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "net.countercraft.movecraft.worldguard"
+            artifactId = "movecraft-worldguard"
+            version = "${project.version}"
+
+            artifact(tasks.jar)
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/apdevteam/movecraft-worldguard")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+hangarPublish {
+    publications.register("plugin") {
+        version.set(project.version as String)
+        channel.set("Release")
+        id.set("Airship-Pirates/Movecraft-WorldGuard")
+        apiKey.set(System.getenv("HANGAR_API_TOKEN"))
+        platforms {
+            register(io.papermc.hangarpublishplugin.model.Platforms.PAPER) {
+                jar.set(tasks.jar.flatMap { it.archiveFile })
+                platformVersions.set(listOf("1.18.2-1.21"))
+                dependencies {
+                    hangar("Movecraft") {
+                        required.set(true)
+                    }
+                    url("WorldGuard", "https://dev.bukkit.org/projects/worldguard") {
+                        required.set(true)
+                    }
+                    hangar("Movecraft-Combat") {
+                        required.set(false)
+                    }
+                }
+            }
+        }
+    }
+}
