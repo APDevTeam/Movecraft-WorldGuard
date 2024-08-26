9fbe89c0b428c59a381fea02ef1486de66562325
Intybyte <vaan.testwork@gmail.com>
2024-08-26 16:19:00 +0200

    Add #8

--- src/main/java/net/countercraft/movecraft/worldguard/listener/ExplosionListener.java
+++ src/main/java/net/countercraft/movecraft/worldguard/listener/ExplosionListener.java
@@ -1,25 +1,46 @@
 package net.countercraft.movecraft.worldguard.listener;
 
 import com.sk89q.worldguard.protection.flags.Flags;
+import net.countercraft.movecraft.MovecraftLocation;
+import net.countercraft.movecraft.craft.CraftManager;
 import net.countercraft.movecraft.events.ExplosionEvent;
+import net.countercraft.movecraft.worldguard.CustomFlags;
 import net.countercraft.movecraft.worldguard.MovecraftWorldGuard;
+import net.countercraft.movecraft.worldguard.utils.WorldGuardUtils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.Location;
 
 public class ExplosionListener implements Listener {
     @EventHandler
     public void onExplosion(ExplosionEvent e) {
-        switch (MovecraftWorldGuard.getInstance().getWGUtils().getState(
-                null, e.getExplosionLocation(), Flags.OTHER_EXPLOSION)) {
-            case ALLOW:
-            case NONE:
-                break; // Other-explosion is allowed
+        var wgUtils = MovecraftWorldGuard.getInstance().getWGUtils();
+        Location explosionLocation = e.getExplosionLocation();
+
+        switch (wgUtils.getState(null, explosionLocation, Flags.OTHER_EXPLOSION)) {
             case DENY:
                 // Other-explosion is not allowed
                 e.setCancelled(true);
                 return;
-            default:
+            case ALLOW: // Other-explosion is allowed
+            case NONE:
                 break;
         }
+
+        WorldGuardUtils.State onlyDamagePilotedCrafts = wgUtils.getState(null, explosionLocation, CustomFlags.ONLY_DAMAGE_PILOTED_CRAFTS);
+        if (onlyDamagePilotedCrafts != WorldGuardUtils.State.ALLOW) {
+            return;
+        }
+
+        MovecraftLocation movecraftLocation = new MovecraftLocation(explosionLocation.getBlockX(), explosionLocation.getBlockY(), explosionLocation.getBlockZ());
+        boolean explodingOnCraft = CraftManager.getInstance().getCrafts().stream().anyMatch(it -> {
+            if (it.getWorld() != explosionLocation.getWorld()) {
+                return false;
+            }
+
+            return it.getHitBox().contains(movecraftLocation);
+        });
+
+        e.setCancelled(!explodingOnCraft);
     }
 }
